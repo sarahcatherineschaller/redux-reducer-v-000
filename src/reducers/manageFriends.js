a8a69c4bd40e2907d98c1e29d1be034d89035a21
Sarah Schaller <sarahcschaller@gmail.com>
2018-10-29 16:34:01 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {
+        ...state,
+        friends: [...state.friends, action.friend]
+      }
+    case 'REMOVE_FRIEND':
+      return {
+        ...state,
+        friends: state.friends.filter(friend => friend.id !== action.id)
+      }
+    default:
+      return state;
+  }
 }
