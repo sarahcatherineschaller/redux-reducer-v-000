7ac610d84e81f0d4c48f2a90bc1536b18a7ac7a6
Sarah Schaller <sarahcschaller@gmail.com>
2018-10-29 16:33:52 +0000

    automatically backed up by learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -5,6 +5,11 @@ export function manageFriends(state, action){
         ...state,
         friends: [...state.friends, action.friend]
       }
+    case 'REMOVE_FRIEND':
+      return {
+        ...state,
+        friends: state.friends.filter(friend => friend.id !== action.id)
+      }
     default:
       return state;
   }
